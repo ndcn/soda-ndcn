b2d9936f1e26dc1f3743e9f0d56d7f2a91b22ada
Olivier <d.olivier@lumc.nl>
2023-01-16 10:47:52 +0100

    Save before shinydashboard -> bs4dash conversion

--- R/soda_visualise_lips.R
+++ R/soda_visualise_lips.R
@@ -4,22 +4,30 @@ library(shinyWidgets)
 #----------------------------------------- Lipidomics data visualisation UI ----
 soda_visualise_lips_ui = function(id) {
   ns = NS(id)
-  shiny::tagList(
-    shiny::fluidRow(
-      # Select plots to display
-      shinyWidgets::checkboxGroupButtons(
-        inputId = ns("show_plots"),
-        label = "Show plots:",
-        status = "primary",
-        choices = c("Class distribution" = "class_distribution",
-                    "Class comparison" = "class_comparison"),
-        selected = NULL
-      )
+  bs4Dash::dashboardBody(
+    tags$style("body { background-color: ghostwhite}"),
+    fluidRow(
+      actionButton("toggle_box", "Toggle Box"),
+      actionButton("remove_box", "Remove Box", class = "bg-danger"),
+      actionButton("restore_box", "Restore Box", class = "bg-success"),
+      actionButton("update_box", "Update Box", class = "bg-primary")
     ),
-    shiny::fluidRow(
-      shiny::uiOutput(outputId = ns("uiPlots"))
+    br(),
+    box(
+      title = textOutput("box_state"),
+      "Box body",
+      id = "mybox",
+      collapsible = TRUE,
+      closable = TRUE,
+      plotOutput("plot")
     )
   )
+
+
+
+
+
+
 }
 
 #------------------------------------- Lipidomics data visualisation server ----
@@ -27,63 +35,55 @@ soda_visualise_lips_server = function(id, r6 = NULL) {
   shiny::moduleServer(
     id,
     function(input, output, session) {
-      
-      output$plot_id_1 = shiny::renderPlot({
-        plot(1:10,
-             1:10)
+      output$plot <- renderPlot({
+        req(!input$mybox$collapsed)
+        plot(rnorm(200))
       })
       
-      # Render box
+      output$box_state <- renderText({
+        state <- if (input$mybox$collapsed) "collapsed" else "uncollapsed"
+        paste("My box is", state)
+      })
       
-      output$uiPlots = renderUI({
-        shiny::tagList(
-          shiny::column(
-            width = 12,
-            shiny::fluidRow(
-              if ("class_distribution" %in% input$show_plots) {
-                bs4Dash::box(
-                  id = "class_distribution_box",
-                  title = "Class distribution",
-                  width = 6,
-                  solidHeader = TRUE,
-                  maximizable = TRUE,
-                  collapsible = FALSE,
-                  status = "primary"
-                  # plotlyOutput(outputId = "plotlyClassDistribution",
-                  #              height = boxDimension$height),
-                  # sidebar = bs4Dash::boxSidebar(
-                  #   id = "classDistributionSidebar",
-                  #   width = 40,
-                  #   uiOutput(outputId = "uiComparisonClassDistribution")
-                  # )
-                )
-              }else{
-                NULL
-              },
-              if ("class_comparison" %in% input$show_plots) {
-                bs4Dash::box(
-                  id = "class_comparison_box",
-                  title = "Class comparison",
-                  width = 6,
-                  solidHeader = TRUE,
-                  maximizable = TRUE,
-                  collapsible = FALSE,
-                  status = "primary"
-                  # plotlyOutput(outputId = "plotlyClassDistribution",
-                  #              height = boxDimension$height),
-                  # sidebar = bs4Dash::boxSidebar(
-                  #   id = "classDistributionSidebar",
-                  #   width = 40,
-                  #   uiOutput(outputId = "uiComparisonClassDistribution")
-                  # )
-                )
-              }else{
-                NULL
-              }
-            )
+      observeEvent(input$toggle_box, {
+        updateBox("mybox", action = "toggle")
+      })
+      
+      observeEvent(input$remove_box, {
+        updateBox("mybox", action = "remove")
+      })
+      
+      observeEvent(input$restore_box, {
+        updateBox("mybox", action = "restore")
+      })
+      
+      observeEvent(input$update_box, {
+        updateBox(
+          "mybox",
+          action = "update",
+          options = list(
+            title = h2("New title", dashboardBadge(1, color = "primary")),
+            status = "danger",
+            solidHeader = TRUE,
+            width = 4
           )
         )
       })
+      
+      observeEvent(input$mybox$visible, {
+        collapsed <- if (input$mybox$collapsed) "collapsed" else "uncollapsed"
+        visible <- if (input$mybox$visible) "visible" else "hidden"
+        message <- paste("My box is", collapsed, "and", visible)
+        toast(
+          title = message,
+          options = list(
+            autohide = TRUE,
+            class = "bg-pink",
+            position = "topRight"
+          )
+        )
+      })
+
     }
   )
 }
